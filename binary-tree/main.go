0802e9a00a4b974686fd4133029ef3fbcc9d5b11
Giovane Aguiar <giovaneaalmeida27@gmail.com>
2022-11-22 13:36:57 -0300

    starts remove feature from binary tree

--- binary-tree/main.go
+++ binary-tree/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
 type Node struct {
@@ -12,6 +11,8 @@ type Node struct {
 	Parent *Node
 }
 
+// Node functions
+
 func (n Node) hasLeftNode() bool {
 	return n.Left != nil
 }
@@ -19,6 +20,34 @@ func (n Node) hasLeftNode() bool {
 func (n Node) hasRightNode() bool {
 	return n.Right != nil
 }
+
+func (n Node) isLeftSon() bool {
+	if n.Parent.Left == &n {
+		return true
+	}
+	return false
+}
+
+func (n Node) isRightSon() bool {
+	if n.Parent.Right == &n {
+		return true
+	}
+	return false
+}
+
+func (n Node) successor() *Node {
+	node := successorRecursively(n.Right)
+	return node
+}
+
+func successorRecursively(node *Node) *Node {
+	if node.hasLeftNode() {
+		successorRecursively(node.Left)
+	}
+	return node
+}
+
+// BTree functions
 
 type BinaryTree struct {
 	Root *Node
@@ -40,39 +69,87 @@ func (b *BinaryTree) insert(newNode *Node, currentNode *Node) {
 
 	if newNode.Value > currentNode.Value {
 		if currentNode.Right == nil {
-			fmt.Printf("inserted %v in the right of %v\n", newNode.Value, currentNode.Value)
+			// fmt.Printf("inserted %v in the right of %v\n", newNode.Value, currentNode.Value)
 			currentNode.Right = newNode
 			newNode.Parent = currentNode
 			return
 		}
 
-		fmt.Printf("called b.insert in the right node of %v\n", currentNode.Value)
+		// fmt.Printf("called b.insert in the right node of %v\n", currentNode.Value)
 		b.insert(newNode, currentNode.Right)
 	}
 
 	if newNode.Value < currentNode.Value {
 		// calls insert wiht the currentNode.left
 		if currentNode.Left == nil {
-			fmt.Printf("inserted %v in the left of %v\n", newNode.Value, currentNode.Value)
+			// fmt.Printf("inserted %v in the left of %v\n", newNode.Value, currentNode.Value)
 			currentNode.Left = newNode
 			newNode.Parent = currentNode
 			return
 		}
-		fmt.Printf("called b.insert in the left node of %v\n", currentNode.Value)
+		// fmt.Printf("called b.insert in the left node of %v\n", currentNode.Value)
 		b.insert(newNode, currentNode.Left)
+	}
+}
+
+func (b *BinaryTree) remove(value int) {
+	var currentNode *Node = b.Root
+	b.removeRecursively(value, currentNode)
+}
+
+func (b *BinaryTree) removeRecursively(value int, currentNode *Node) {
+	if value > currentNode.Value {
+		b.removeRecursively(value, currentNode.Right)
+	}
+	if value < currentNode.Value {
+		b.removeRecursively(value, currentNode.Left)
+	}
+
+	if value == currentNode.Value {
+		if currentNode == b.Root {
+			b.Root = nil
+
+		}
+
+		if !currentNode.hasRightNode() && !currentNode.hasLeftNode() {
+			currentNode = nil
+		}
+
+		if currentNode.hasRightNode() && !currentNode.hasLeftNode() {
+			currentNode.Right.Parent = currentNode.Parent
+
+			if currentNode.isLeftSon() {
+				currentNode.Parent.Left = currentNode.Right
+			} else {
+				currentNode.Parent.Right = currentNode.Right
+			}
+		}
+
+		if currentNode.hasLeftNode() && !currentNode.hasRightNode() {
+			currentNode.Left.Parent = currentNode.Parent
+
+			if currentNode.isRightSon() {
+				currentNode.Parent.Right = currentNode.Left
+			} else {
+				currentNode.Parent.Left = currentNode.Left
+			}
+		}
+
+		// TODO: node with two sons
+
 	}
 }
 
 func (b *BinaryTree) inOrderTransversal() {
 	node := b.Root
-	b.inOrderTransversalRecursivily(node)
+	b.inOrderTransversalRecursively(node)
 }
 
 func (b *BinaryTree) inOrderTransversalRecursively(node *Node) {
 	if node != nil {
-		b.inOrderTransversalRecursivily(node.Left)
+		b.inOrderTransversalRecursively(node.Left)
 		fmt.Println(node.Value)
-		b.inOrderTransversalRecursivily(node.Right)
+		b.inOrderTransversalRecursively(node.Right)
 	}
 }
 
@@ -87,11 +164,35 @@ func CreateTree(rootValue int) *BinaryTree {
 func main() {
 	tree := CreateTree(10)
 
-	for i := 0; i < 10; i++ {
-		var node Node
-		node.Value = rand.Intn(50)
-		tree.insertRecursivily(&node)
-	}
+	var node1 Node
+	var node2 Node
+	var node3 Node
+	var node4 Node
+	var node5 Node
+	var node6 Node
+	var node7 Node
+	var node8 Node
 
+	node1.Value = 10
+	node2.Value = 8
+	node3.Value = 9
+	node4.Value = 12
+	node5.Value = 11
+	node6.Value = 16
+	node7.Value = 14
+	node8.Value = 19
+
+	tree.insertRecursively(&node1)
+	tree.insertRecursively(&node2)
+	tree.insertRecursively(&node3)
+	tree.insertRecursively(&node4)
+	tree.insertRecursively(&node5)
+	tree.insertRecursively(&node6)
+	tree.insertRecursively(&node7)
+	tree.insertRecursively(&node8)
+
+	// tree.inOrderTransversal()
+
+	tree.remove(16)
 	tree.inOrderTransversal()
 }
